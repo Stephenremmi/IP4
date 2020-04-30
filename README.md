ff182aa4fe7d34a2f4bb1b57e6d15f943d9f2a7e
stephen remmi <stephenremmi@gmail.com>
2020-04-30 13:25:49 +0300

    Finalize Readme.md and add functionality

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
-# IP4
-independent project 4
+# Pizzaria
++ This is website that enable customers to order pizza at any geolocation at affordable prizes.
+
+## Technology and versions
+Languages,libraries and frameworks used include;
++ HTML5
++ CSS 3
++ JAVASCRIPT
++ JQUERY 3.4.1
++ BOOTSTRAP 4
+
+### Description
+The website is user friendly and with wide range of selections in every pizza category.
+It also offers various online cash transfer methods for faster, cheaper , and convenient mobile transactions.
+subscribers get to be notified on a weekly basis on pizza discounts and job opportunities in our branches
+
+## Live link to webpage
+Here is a link to the website:https://stephenremmi.github.io/IP4/
+
+## Contact details
+In case of any trouble with the API email me at:stephenremmi@gmail.com
+Feel free to make changes.
+
+## Screenshots
+!![] (img/sc1.png)
+!![] (img/sc2.png)
+!![] (img/sc3.png)
+!![] (img/sc4.png)
+!![] (img/sc5.png)
--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,78 @@
+$("#order").click(function(){
+  
+  var medium = document.getElementById("medium");
+  var large = document.getElementById("large");
+  var crispy = document.getElementById("crispy");
+  var stuffed = document.getElementById("stuffed");
+  var glutenFree = document.getElementById("glutenFree");
+  var pepperoni = document.getElementById("pepperoni");
+  var onions = document.getElementById("onions");
+  var bacon = document.getElementById("bacon");
+  var extraCheese = document.getElementById("extraCheese");
+  var pineApple = document.getElementById("pineapple");
+  var numberOfPizzas = document.getElementById("number");
+  var mpesa = document.getElementById("mce-MMERGE5-0");
+  var bank = document.getElementById("mce-MMERGE5-1");
+  var paypal = document.getElementById("mce-MMERGE5-2");
+  
+  
+  if(small.checked == false && medium.checked ==false && large.checked ==false){
+    alert("check pizza size")
+  }
+  else if(mpesa.checked == false && bank.checked == false && paypal.checked ==false){
+    alert("Amount not recieved")
+  }
+  else if(
+   prompt("Enter location")){
+    alert("click the checkout button to view total amount")
+   }
+
+  else{
+     swal({
+       icon: "warning",
+       text: "Enter location for delivery to be made",
+       title:"PIZZARIA ORDERS",
+       button:"Oops!!"
+     })
+    }
+});
+  
+$("#checkout").click(function(){
+  
+  var small = document.getElementById("small");
+  var medium = document.getElementById("medium");
+  var large = document.getElementById("large");
+  var crispy = document.getElementById("crispy");
+  var stuffed = document.getElementById("stuffed");
+  var glutenFree = document.getElementById("glutenFree");
+  var pepperoni = document.getElementById("pepperoni");
+  var onions = document.getElementById("onions");
+  var bacon = document.getElementById("bacon");
+  var extraCheese = document.getElementById("extraCheese");
+  var pineApple = document.getElementById("pineapple");
+  var numberOfPizzas = document.getElementById("number");
+  var mpesa = document.getElementById("mce-MMERGE5-0");
+  var bank = document.getElementById("mce-MMERGE5-1");
+  var paypal = document.getElementById("mce-MMERGE5-2");
+  
+  
+  if(small.checked == false && medium.checked ==false && large.checked ==false){
+    alert("check pizza size")
+  }
+  else if(mpesa.checked == false && bank.checked == false && paypal.checked ==false){
+    alert("Amount not recieved")
+  }
+  else{
+    swal({
+      icon: "success",
+      text: "Total cost of delivery is 1500/=",
+      title:"PIZZARIA ORDERS"
+    })
+  }
+});
+  
+
+  
 
   var modal1 = document.getElementById("myModal1");
   var modal2 = document.getElementById("myModal2");
